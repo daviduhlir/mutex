b434f083d0a3651c18d99a698edf3d55b2248e34
David Uhlíř <posta@daviduhlir.cz>
2022-05-24 13:25:45 +0200

    updated example

---
+++ README.md
@@ -0,0 +1,49 @@
+# Mutex for node.js clustered applications
+
+This is simple mutex, that can be used between workers and will lock/unlock it using IPC messages.
+It provides 2 types of locks, single-access and multi-access.
+
+Multi-access means, multiple scopes can be opened - it's there for reading. If some part of code will lock same key with single access, all scopes will wait for it.
+Single-access means, only one scope can be opened in time, all other locks with same key will wait until unlock.
+
+```
+npm install @david.uhlir/mutex
+```
+
+## Usage
+
+``` ts
+import { SharedMutex } from '@david.uhlir/mutex';
+
+SharedMutex.lockSingleAccess('test', async () => {
+    console.log('Lock test 1')
+    await delay(1000)
+    console.log('Unlock test 1')
+})
+
+SharedMutex.lockSingleAccess('test', async () => {
+    console.log('Lock test 2')
+    await delay(1000)
+    console.log('Unlock test 2')
+})
+
+```
+
+``` ts
+import { SharedMutex } from '@david.uhlir/mutex';
+
+SharedMutex.lockMultiAccess('test', async () => {
+    console.log('Lock test 1')
+    await delay(1000)
+    console.log('Unlock test 1')
+})
+
+SharedMutex.lockMultiAccess('test', async () => {
+    console.log('Lock test 2')
+    await delay(1000)
+    console.log('Unlock test 2')
+})
+
+```
+
+MIT
--- example/src/index.ts
+++ example/src/index.ts
@@ -1,30 +1,44 @@
 import { SharedMutex } from '@david.uhlir/mutex';
-import * as cluster from 'cluster'
 
 function delay(time: number) {
   return new Promise((resolve) => setTimeout(resolve, time))
 }
 
-async function test(name: string, fnc?: () => Promise<void>) {
-  await SharedMutex.lockSingleAccess(name, async () => {
-    console.log(process.env.i, 'Lock ' + name)
-    if (fnc) {
-      await fnc()
-    }
-  })
-  console.log(process.env.i, 'Unlock ' + name)
-}
+(async function() {
+  console.log('----Running single access test----\n')
 
-if (cluster.isMaster) {
-  for (let i = 0; i < 5; i++) {
-    cluster.fork({i})
-  }
-} else {
-  (async function() {
-    await test('root/test/' + process.env.i, async () => {
-      await test('root/test/nested', async () => {
+  await Promise.all([
+    SharedMutex.lockSingleAccess('mutex', async () => {
+        console.log('Lock single test 1')
         await delay(1000)
-      })
+        console.log('Unlock single test 1')
+    }),
+    SharedMutex.lockSingleAccess('mutex', async () => {
+        console.log('Lock single test 2')
+        await delay(1000)
+        console.log('Unlock single test 2')
     })
-  })()
-}
+  ])
+
+  console.log('\n----Running multi access test----\n')
+
+  await Promise.all([
+    SharedMutex.lockMultiAccess('mutex', async () => {
+        console.log('Lock multi test 1')
+        await delay(1000)
+        console.log('Unlock multi test 1')
+    }),
+    SharedMutex.lockMultiAccess('mutex', async () => {
+        console.log('Lock multi test 2')
+        await delay(1000)
+        console.log('Unlock multi test 2')
+    }),
+    SharedMutex.lockSingleAccess('mutex', async () => {
+      console.log('Lock single test 2')
+      await delay(1000)
+      console.log('Unlock single test 2')
+  })
+  ])
+})()
+
+

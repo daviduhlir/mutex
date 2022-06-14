05ddf3f1bb24a6aba44bc4970edb43b3a9fa4943
DavidUhlíř <posta@daviduhlir.cz>
2022-06-14 14:43:07 +0200

    connected secondary sync interface

--- lib/SecondarySynchronizer.ts
+++ lib/SecondarySynchronizer.ts
@@ -1,20 +1,23 @@
 import { LocalLockItem } from './interfaces'
+import { EventEmitter } from 'events'
 
-export class SecondarySynchronizer {
-    constructor() {
-
-    }
+export const SYNC_EVENTS = {
+    LOCK: 'LOCK',
+    UNLOCK: 'UNLOCK',
+    CONTINUE: 'CONTINUE'
+}
+export class SecondarySynchronizer extends EventEmitter {
 
     public lock(item: LocalLockItem) {
-        
+
     }
 
     public unlock(hash: string) {
-        
+
     }
 
     public continue(item: LocalLockItem) {
-        
+
     }
 
     public get isArbitter(): boolean {

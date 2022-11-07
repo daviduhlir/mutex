0a823796e2f88cf249ef8770302f97e8d523e2c2
DavidUhlíř <posta@daviduhlir.cz>
2022-11-07 16:57:58 +0100

    exports fix

--- lib/index.ts
+++ lib/index.ts
@@ -3,6 +3,7 @@ import { SharedMutexSynchronizer } from './SharedMutexSynchronizer'
 export * from './SharedMutex'
 export * from './SharedMutexDecorators'
 export * from './SecondarySynchronizer'
+export * from './SharedMutexSynchronizer'
 
 if (cluster.isMaster) {
   SharedMutexSynchronizer.initializeMaster()
